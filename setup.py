31b2a0f277dc6ae1d94f87479f82debd674d5b24
tombch <t.brier@outlook.com>
2023-06-12 16:21:40 +0100

    Changed folder name back to onyx. Thats the last time I do this

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
-exec(open("onyx_client/version.py").read())
+exec(open("onyx/version.py").read())
 
 setuptools.setup(
     name="onyx",
     author="Thomas Brier",
     version=__version__,  # type: ignore
     packages=setuptools.find_packages(),
-    entry_points={"console_scripts": "onyx = onyx_client.cli:main"},
+    entry_points={"console_scripts": "onyx = onyx.cli:main"},
     install_requires=[
         "requests",
         "django-query-tools",
