e2f31201c3587b8445c094d24f7f513361a0e3cb
tombch <t.brier@outlook.com>
2023-08-15 16:06:17 +0100

    Added requests.HTTPError to client library, and removed utils

--- onyx/__init__.py
+++ onyx/__init__.py
@@ -1,5 +1,5 @@
 """🧿"""
 from .config import OnyxConfig
 from .api import OnyxClient
-from .utils import print_response, execute_uploads
 from django_query_tools.client import F
+from requests import HTTPError
