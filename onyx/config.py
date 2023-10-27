216c2be903c0bbdead024dd48e91c5204aa7b1bf
tombch <t.brier@outlook.com>
2023-10-27 19:08:46 +0100

    OnyxEnv Enum for environment variables, and simplified OnyxConfig by removing file based config

--- onyx/config.py
+++ onyx/config.py
@@ -1,66 +1,52 @@
 import os
-import json
-from typing import Optional, Union
+import enum
+from typing import Optional
 
-# TODO: Appropriate error messages for stuff such as domain not existing (i.e. when there is no config file)
-# TODO: Ok as first step. refactor to do what Andy said and pull out file management
+
+class OnyxEnv(enum.Enum):
+    DOMAIN = "ONYX_DOMAIN"
+    USERNAME = "ONYX_USERNAME"
+    PASSWORD = "ONYX_PASSWORD"
+    TOKEN = "ONYX_TOKEN"
+    DISPLAY = "ONYX_DISPLAY"
 
 
 class OnyxConfig:
-    __slots__ = "config_path", "domain", "username", "password", "token"
-    CONFIG_FILE_PATH = "~/.onyx"
+    """
+    Class for storing information required to connect/authenticate with Onyx.
+    """
 
     def __init__(
         self,
-        config_path: Optional[str] = None,
         domain: Optional[str] = None,
+        credentials: Optional[tuple[str, str]] = None,
         token: Optional[str] = None,
-        username: Optional[str] = None,
-        password: Optional[str] = None,
     ) -> None:
-        self.domain = domain
-        self.token = token
-
-        if config_path:
-            self.config_path = config_path
-        else:
-            self.config_path = OnyxConfig.CONFIG_FILE_PATH
-
-        self.config_path = self.config_path.replace("~", os.path.expanduser("~"))
-
-        if os.path.isfile(self.config_path):
-            with open(self.config_path) as config_file:
-                config = json.load(config_file)
-
-                if not self.domain and config.get("domain"):
-                    self.domain = str(config["domain"])
-
-                if not self.token and config.get("token"):
-                    self.token = str(config["token"])
-        else:
-            if not self.domain:
-                raise Exception("Could not find domain name.")
-
-        self.username = username
-        self.password = password
-
-    def write_token(self, token: Union[str, None]) -> None:
-        """Update the tokens file for `username`.
-
-        Parameters
-        ----------
-        token : str, optional
-            The token being written to their tokens file.
+        """
+        Initialise the config.
         """
 
-        if os.path.isfile(self.config_path):
-            with open(self.config_path, "w") as config_file:
-                config = {
-                    "domain": self.domain,
-                    "token": token,
-                }
-                json.dump(
-                    config,
-                    config_file,
-                    indent=4,
-                )
+        if not domain:
+            domain = os.environ[OnyxEnv.DOMAIN.value]
+
+        if not credentials:
+            username = os.getenv(OnyxEnv.USERNAME.value)
+            password = os.getenv(OnyxEnv.PASSWORD.value)
+
+            if username and password:
+                credentials = (username, password)
+
+        if not token:
+            token = os.getenv(OnyxEnv.TOKEN.value)
+
+        self.domain = domain
+        self.credentials = credentials
+        self.token = token
+
+        if not self.domain:
+            raise Exception("Must provide a domain to connect to Onyx.")
+
+        if not (self.credentials or self.token):
+            raise Exception(
+                "Must provide either credentials (username, password) or a token to authenticate with Onyx."
+            )
